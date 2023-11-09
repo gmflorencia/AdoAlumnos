5f8be6e04d59d7d964b43f0140ded47d344c84a7
macabrera <macabrera@andreani.com>
2023-11-09 19:18:12 -0300

    modificaciones

--- Vista/frmLogin.Designer.cs
+++ Vista/frmLogin.Designer.cs
@@ -93,7 +93,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.BtnAceptar);
             this.Controls.Add(this.LblPassword);
